990eec72f28c55618226cc79cebbdd6512e13792
teganGowing <tgowing97@gmail.com>
2021-08-25 11:02:45 +0100

    help

--- dbconfig/1_create_posts.sql
+++ dbconfig/1_create_posts.sql
@@ -4,7 +4,7 @@ CREATE TABLE posts (
     id serial PRIMARY KEY,
     title VARCHAR (100),
     date date DEFAULT GETDATE(),
-    psuedonym VARCHAR (50),
+    psuedonym VARCHAR (50) DEFAULT anonymous,
     body VARCHAR (600),
 );
 

88c48755801ad679de438a93aa278202f405ca0f
Abigail McLeod-Peck <abigail.m.peck@gmail.com>
2021-08-25 12:15:06 +0100

    Added posts creation

--- api/models/Post.js
+++ api/models/Post.js
@@ -22,23 +22,34 @@ module.exports = class Post {
     });
   }
 
-  // static findById(id) {
-  //   return new Promise(async (resolve, reject) => {
-  //     try {
-  //       // let postData = await db.query("SELECT * FROM post WHERE id = $1;", [
-  //       //   id,
-  //       // ]);
-  //       let post = new Post({
-  //         id: 2,
-  //         title: "hahah",
-  //         date: 2345,
-  //         pseudonym: "egwerg",
-  //         body: "gwerngoweurhgoiwjerg",
-  //       });
-  //       resolve(post);
-  //     } catch (err) {
-  //       reject("Post not found");
-  //     }
-  //   });
-  // }
+  static findById(id) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let postData = await db.query(
+          `SELECT * FROM posts WHERE posts.id = $1;`,
+          [id]
+        );
+        let post = new Post(postData.rows[0]);
+        resolve(post);
+      } catch (err) {
+        reject("Post not found");
+      }
+    });
+  }
+
+  static async create(Data) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const { title, pseudonym, body } = postData;
+        const result = await db.query(
+          "INSERT INTO posts (title, pseudonym, body) VALUES ($1, $2, $3) RETURNING id",
+          [title, pseudonym, body]
+        );
+        resolve(result.rows[0]);
+      } catch (err) {
+        console.log(err);
+        reject("Post could not be created");
+      }
+    });
+  }
 };
